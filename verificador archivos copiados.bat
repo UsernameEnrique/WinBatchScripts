7bd4a0f7240d9252ce2dcd5ce1509996eed783af
UsernameEnrique <alejandroenriquelive@hotmail.com>
2025-12-16 12:36:33 -0600

    path_writer
    función agregada para escribir las rutas y no cambiarlas en codigo

--- verificador archivos copiados.bat
+++ verificador archivos copiados.bat
@@ -2,9 +2,14 @@
 setlocal EnableExtensions
 
 REM === Define tus rutas reales aquí ===
-set "ORIGEN=C:\Carpeta\de\origen"
-set "DESTINO=C:\Carpeta\de\destino"
-set "RUTALOG=C:\Ruta\del\log"
+set /p "ORIGEN=Introduce la ruta de ORIGEN: "
+set /p "DESTINO=Introduce la ruta de DESTINO: "
+set /p "RUTALOG=Introduce la ruta del archivo de log: "
+
+REM Quita comillas si el usuario las puso
+set ORIGEN=%ORIGEN:"=%
+set DESTINO=%DESTINO:"=%
+set RUTALOG=%RUTALOG:"=%
 
 REM === Validaciones básicas ===
 if not defined ORIGEN (
@@ -25,30 +30,33 @@ if not defined RUTALOG (
 REM Normaliza a ruta absoluta (si aplica)
 for %%A in ("%ORIGEN%") do set "ORIGEN=%%~fA"
 for %%A in ("%DESTINO%") do set "DESTINO=%%~fA"
+for %%A in ("%RUTALOG%") do set "RUTALOG=%%~fA"
 
-REM Verifica que no sean iguales
-if /I "%ORIGEN%"=="%DESTINO%" (
-  echo ERROR: ORIGEN y DESTINO son la misma ruta: "%ORIGEN%"
+REM Verifica que los destinos no sean iguales
+if /I "%ORIGEN%"=="%DESTINO%"=="%RUTALOG%" (
+  echo ERROR: ORIGEN, DESTINO y RUTALOG no pueden tener la misma ruta.
   goto :fin
 )
 
 REM Verifica que existan como carpetas
-if not exist "%ORIGEN%\NUL" (
+if not exist "%ORIGEN%\" (
   echo ERROR: La carpeta ORIGEN no existe: "%ORIGEN%"
   goto :fin
 )
 
-if not exist "%DESTINO%\NUL" (
+if not exist "%DESTINO%\" (
   echo ERROR: La carpeta DESTINO no existe: "%DESTINO%"
   goto :fin
 )
 
-if not exist "%RUTALOG%" (
-  echo ERROR: El archivo de log no existe: "%RUTALOG%"
+if not exist "%RUTALOG%\" (
+  echo ERROR: La carpeta del archivo de log no existe: "%RUTALOG%"
   goto :fin
 )
 
 REM === Ejecucion ===
+set RUTALOG=%RUTALOG%\verificacion.txt
+REM type nul > "%RUTALOG%"
 robocopy "%ORIGEN%" "%DESTINO%" /MIR /L /FFT /R:0 /W:0 /XJ /NP /LOG:"%RUTALOG%"
 echo Robocopy termino con ERRORLEVEL=%ERRORLEVEL%
 
